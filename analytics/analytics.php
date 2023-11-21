5e6b769e350b9ad46e8985c0ee2b14954af95099
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2023-11-21 17:21:11 +0700

    session added

--- analytics/analytics.php
+++ analytics/analytics.php
@@ -1,3 +1,12 @@
+<?php include "../average.php"; ?>
+
+<?php 
+session_start(); 
+if(!isset($_SESSION['user_id'])){ 
+    header('Location: ../index.php'); 
+    exit;
+}
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
