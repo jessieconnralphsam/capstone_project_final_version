e6ac5b3fa2e16eb4bebfe97093b62e5ba3a5ccbf
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2023-12-26 15:05:48 +0800

    report_done

--- php/temperature.php
+++ php/temperature.php
@@ -231,7 +231,7 @@
           </nav>
           <div class="content-wrapper">
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Hydroponics /</span> Fourly Water Flow Readings</h4>
+              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Hydroponics /</span> Fourly Temperature Readings</h4>
               <div class="table-responsive text-nowrap">
                 <table id="datatables" class="table card-table">
                   <thead>
